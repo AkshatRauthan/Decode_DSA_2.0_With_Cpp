82e3b5e7967ab00c7b920947ecc3f5beacbff24a
Akshat_Rauthan <141433398+AkshatRauthan@users.noreply.github.com>
2023-10-11 04:02:42 +0530

    Initial commit

--- Lecture22_SelectionAndInsertionSort/insertionSortAlgorithm.cpp
+++ Lecture22_SelectionAndInsertionSort/insertionSortAlgorithm.cpp
@@ -2,8 +2,15 @@
 #include "array"
 using namespace std;
 
-//                                       ________Insertion Sort________
-//
+//                                     ________Insertion Sort________
+// Algorithm :- In Every Round Swap The nth Element With Its Previous One Until The Element Is Smaller Than
+// Previous One After Starting All The Rounds From The Second Index.
+// Here We Have Initially Divided Our Array Into Two Parts : The Sorted Part Containing The First Element And
+// The Unsorted Containing All The Remaining Elements.
+// Now After Every Round The length Of The Sorted Part Increases By 1 And The Size Of The Unsorted Part Decreases
+// By 1.
+// All This Will Continue Until The Length OF The Unsorted Part Becomes Zero.
+// Remember That We Are Not Actually Dividing Our Array, It Is Just A Notation USed To Address Them.
 
 int main(){
     int n;
@@ -16,6 +23,13 @@ int main(){
     for (int i=0; i<n; i++) cout<<arr[i]<<"  ";
 
     // Insertion Sort Algorithm :-
-
-
+    for (int i=1; i<n; i++){
+        for (int j=i; arr[j-1] > arr[j] && j>0;j--){
+            swap(arr[j-1],arr[j]);
+        }
+    }
+    cout<<"\n\nThe Elements of The Array In Sorted Order Are : \n";
+    for (int i : arr) cout<<i<<"  ";
+    printf("\n\n");
+    system("pause");
 }
