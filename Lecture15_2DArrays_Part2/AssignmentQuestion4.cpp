a04bf62cea1084ca5bcfbec0f796844664e190c7
Akshat_Rauthan <141433398+AkshatRauthan@users.noreply.github.com>
2023-10-15 19:41:19 +0530

    Initial commit

--- Lecture15_2DArrays_Part2/AssignmentQuestion4.cpp
+++ Lecture15_2DArrays_Part2/AssignmentQuestion4.cpp
@@ -1,6 +1,6 @@
 #include "iostream"
 using namespace std;
-// Incomplete
+
 //  Given a positive integer n, generate n*n matrix filled with elements from 1 to n2 in spiral order.
 //  Input 1: n = 3
 //  Output 1: [[1,2,3],[8,9,4],[7,6,5]]
@@ -11,36 +11,31 @@ int main(){
     int n;
     cout<<"\nEnter The Order Of The Square Matrix :\n";
     cin>>n;
-    int arr[n][n];
-    int br=0,nr=n-1,bc=0,nc=n-1;
-    for (int count=1;count<=n*n;count++){
-        // Left To Right
-        for(int i=bc;i<nc;i++) {
-            arr[br][i]=count;
-            count++;
-        } br++;
 
-        // Top To Bottom
-        for(int i=br;i<nr && count<=n*n;i++){
-            arr[i][nc]=count;
-            count++;
-        } nc--;
+    int minr=0, maxr=n-1, minc=0, maxc=n-1, count=1;
+    int v[n][n];
+    while (count <= n*n){
 
-        // Right To Left
-        for(int i=nc;i>bc && count<=n*n;i--){
-            arr[nr][i]=count;
-            count++;
-        } nr--;
+        // Top Row
+        for (int i=minc;i<=maxc && count<=n*n;i++,count++) v[minr][i] = count;
+        minr++;
 
-        // Bottom To Top
-        for(int i=nr;i>br && count<=n*n;i--) arr[i][bc]=count;
-        bc++;
+        // Rightmost Column
+        for (int i=minr;i<=maxr && count<=n*n;i++,count++) v[i][maxc] = count;
+        maxc--;
+
+        // Bottom Row In Reverse
+        for (int i=maxc;i>=minc && count<=n*n;i--,count++) v[maxr][i] = count;
+        maxr--;
+
+        // Leftmost Column In Reverse
+        for (int i=maxr;i>=minr && count<=n*n;i--,count++) v[i][minc] = count;
+        minc++;
     }
+
     cout<<"\nThe Required Matrix Is As Follows \n";
-    for (int i=0;i<n;i++){
-        for (int j=0;j<n;j++){
-            cout<<arr[i][j]<<"  ";
-        }
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++) cout<<v[i][j]<<"  ";
         cout<<"\n";
     }
     cout<<"\n\n";
