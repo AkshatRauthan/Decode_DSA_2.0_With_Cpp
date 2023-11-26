7311f462699fef832a1300900668af5235270940
Akshat_Rauthan <141433398+AkshatRauthan@users.noreply.github.com>
2023-11-26 15:22:24 +0530

    Initial commit

--- Lecture37_Oops_Introduction/Classes_Introduction.cpp
+++ Lecture37_Oops_Introduction/Classes_Introduction.cpp
@@ -5,15 +5,23 @@ using namespace std;
 class player{
 
 private:            // Access Specifier
-    int health;
-    int score;      // Data Fields Or Attributes
+    int health;     // Data Fields Or Attributes
+    int score;
     string name;
 
 public:
-    player(string name, int health, int score){    //
+    player (string name, int health, int score){
+        // Constructor Of Class Player :
+        // Here Player Class Is The Return Type Of The Constructor.
+        // They Do Not Have Any Name.
+        // When Called Returns An Object Of The Class In Which They Are Present.
         this -> health = health;  // This : Generic Pointer To Passed Variable Present In Parent Class.
         this -> name = name;      // Therefore, This Statement Put The Values Passed In Function To Their
         this -> score = score;    // Respective Data Fields Of The Current Object Of Parent Class.
+    }
+
+    ~player (){      // Destructor For Class Player.
+        cout<<"Destructor Called For Player "<<name<<"\n";
     }
 
     void stats(){   // Getter Function
@@ -25,7 +33,7 @@ public:
 
 int main(){
     // Alan : An Object Belonging To Player Class.
-    player alan("Alan", 1000, 250);
+    player alan("Alan", 1000, 250);     // Creating Object Using Constructors.
     alan.stats();
-    cout<<"\n\n";
+    cout<<"\nAAA\n";
 }
